7ad0df5a7dc2f0c9730d2f93bc2cf2f404c38a63
dolfun <abhinav150104@gmail.com>
2026-01-19 06:09:27 +0530

    Remove submodules and use FetchContent instead

--- src/main.cc
+++ src/main.cc
@@ -1,13 +1,10 @@
 #include <benchmark/benchmark.h>
 
 static void BM_SomeFunction(benchmark::State& state) {
-  // Perform setup here
   for (auto _ : state) {
-    // This code gets timed
     int x = 2;
   }
 }
-// Register the function as a benchmark
+
 BENCHMARK(BM_SomeFunction);
-// Run the benchmark
 BENCHMARK_MAIN();
